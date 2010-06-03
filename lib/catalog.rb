48c2c45a7faae442a9489001ed3c034ef0ed5c2c
Brian Stevens <brian@stevens.com>
2010-06-03 15:06:56 -0500

    Added GFPUBLIC_USERID to db connect string

--- lib/catalog.rb
+++ lib/catalog.rb
@@ -5,10 +5,11 @@ class Catalog
   def initialize
     ENV['TWO_TASK'] or raise "ERROR: TWO_TASK environment variable not set!"
     ENV['BASELINE_ACC'] or raise "ERROR: BASELINE_ACC environment variable not set!"
+    ENV['GFPUBLIC_USERID'] ||= 'gf_public:gf_public'
   end
 
   def projects
-    db = Sequel.connect "oracle://gf_public:gf_public@#{ENV[:TWO_TASK]}"
+    db = Sequel.connect "oracle://#{ENV[:GFPUBLIC_USERID]}@#{ENV[:TWO_TASK]}"
     db[:finder_accounts]
       .select(:project_name, :account_name)
       .where(~{:type => ['PROJSYS', 'SYSTEM']})
