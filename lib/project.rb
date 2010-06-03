6e2e163f76f570b06fe7db8f40115a023da150f6
Brian Stevens <brian@stevens.com>
2010-06-03 15:20:58 -0500

    Added Project#bulk_server_command

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,19 @@
+require 'sequel'
+
 class Project
   attr_accessor :name, :account
 
   def initialize(name, account)
     @name = name
     @account = account
+  end
+
+  def bulk_server_command
+    Sequel.connect("oracle://#{@account}:tigers@gfprod") do |db|
+      db[:parameter]
+        .select(:value_string)
+        .where(:code => 'Bulk_Server_Command')
+        .first[:value_string]
+    end
   end
 end
--- test/project_test.rb
+++ test/project_test.rb
@@ -13,4 +13,9 @@ class ProjectTest < Test::Unit::TestCase
   def test_initialize_succeeds_with_name_and_account
     assert_nothing_raised { Project.new(:name, :account) }
   end
+
+  def test_bulk_server_command
+    project = Project.new(:nova_atchmrg, :NOVA_ATCHMRG)
+    assert_equal 'LOCAL:NOVA_ATCHMRG@hesz04.internal.houstonenergyinc.com:/apps/geoframe/geoframe_44_sun/bin/ctsrvr_init.csh', project.bulk_server_command
+  end
 end
