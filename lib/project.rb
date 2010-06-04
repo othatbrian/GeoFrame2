75c445b9791c8b6d208e174d9337473093955d0d
Brian Stevens <brian@stevens.com>
2010-06-04 16:32:07 -0500

    Added lots of error handling for connecting to database

--- lib/project.rb
+++ lib/project.rb
@@ -1,25 +1,100 @@
+require 'open3'
 require 'sequel'
 
 class Project
   attr_accessor :name, :account
+  attr_writer :password
 
-  def initialize(name, account)
+  def initialize(name, account, password=nil)
     @name = name
     @account = account
+    @password = password
   end
 
   def bulk_server_command
-    Sequel.connect("oracle://#{@account}:tigers@gfprod") do |db|
-      db[:parameter]
-        .select(:value_string)
-        .where(:code => 'Bulk_Server_Command')
-        .first[:value_string]
+    @db[:parameter]
+      .select(:value_string)
+      .where(:code => 'Bulk_Server_Command')
+      .first[:value_string]
+  end
+
+  def connect
+    @db = _connect_with_supplied_password
+    begin
+      @db if @db.test_connection
+    rescue Sequel::DatabaseConnectionError => error
+      if @password.downcase != @name.downcase
+        @db = _connect_with_default_password
+        begin
+          @db if @db.test_connection
+        rescue Sequel::DatabaseConnectionError => error
+          raise error
+        end
+      end
+#      if ENV[:GFDBA_PASSWORD] do
+#        Open3.popen3("proj_get_password #{@name} bogus") do |stdin, stdout, stderr|
+#          errors = stderr.read
+#          stdout.read =~ /^Password =(.*)$/m
+#        @db = Sequel.connect("oracle://#{@account}:#{ENV[:GFDBA_PASSWORD]}@#{ENV[:TWO_TASK]}")
+#      else
+#        raise "ERROR: project or password incorrect and GFDBA_PASSWORD not set!"
     end
   end
 
   def super_server_command
-    Sequel.connect("oracle://#{@account}:tigers@gfprod") do |db|
-      db[:project].select(:super_server_command).first[:super_server_command]
+    @db[:project]
+      .select(:super_server_command)
+      .first[:super_server_command]
+  end
+
+  private
+  def _connect_with_supplied_password
+    db = Sequel.connect("oracle://#{@account}:#{@password}@#{ENV[:TWO_TASK]}")
+    begin
+      db if db.test_connection
+    rescue
+      if @password != @name
+        begin
+          _connect_with_default_password
+        rescue
+          raise "password supplied is not valid, " + $!.to_s
+        end
+      else
+        begin
+          _connect_with_gfdba_password
+        rescue
+          raise "password supplied is not valid and " + $!.to_s
+        end
+      end
+    end
+  end
+
+  def _connect_with_default_password
+    db = Sequel.connect("oracle://#{@account}:#{@account}@#{ENV[:TWO_TASK]}")
+    begin
+      db if db.test_connection
+    rescue Sequel::DatabaseConnectionError => error
+      begin
+        _connect_with_gfdba_password
+      rescue
+        raise "default password for #{@name} is not valid and " + $!.to_s
+      end
+    end
+  end
+
+  def _connect_with_gfdba_password
+    raise "GFDBA_PASSWORD is not set" unless ENV[:GFDBA_PASSWORD]
+    begin
+      Open3.popen3("proj_get_password #{@name} password") do |stdin, stdout, stderr|
+        errors = stderr.read
+        raise 'GFDBA_PASSWORD is not valid' if errors =~ /^Invalid password/m
+        stdout.read =~ /^Password =(.*)$/m
+        $1 or raise errors
+        db = Sequel.connect("oracle://#{@account}:#{$1.chomp}@#{ENV[:TWO_TASK]}")
+        db if db.test_connection
+      end
+    rescue Errno::ENOENT
+      raise 'unable to run proj_get_password'
     end
   end
 end
--- test/project_test.rb
+++ test/project_test.rb
@@ -2,6 +2,12 @@ require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/project'
 
 class ProjectTest < Test::Unit::TestCase
+  ENV[:GFDBA_PASSWORD] = nil
+
+  def setup
+    @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ, :tigers)
+  end
+
   def test_initialize_without_name_fails
     assert_raises(ArgumentError) { Project.new }
   end
@@ -14,13 +20,120 @@ class ProjectTest < Test::Unit::TestCase
     assert_nothing_raised { Project.new(:name, :account) }
   end
 
+#  def test_connect
+#    assert @p.connect
+#  end
+
+  def test_connect_with_supplied_password
+    @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ, :tigers)
+    assert_nothing_raised { @p.send :_connect_with_supplied_password }
+  end
+
+  def test_connect_with_invalid_supplied_password
+    @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ, :password)
+    assert_block do
+      begin
+        @p.send :_connect_with_supplied_password
+      rescue
+        $!.to_s == 'password supplied is not valid, default password for wts_work_proj is not valid and GFDBA_PASSWORD is not set'
+      end
+    end
+  end
+
+  def test_connect_with_invalid_supplied_password_same_as_default
+    @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ, :wts_work_proj)
+    assert_block do
+      begin
+        @p.send :_connect_with_supplied_password
+      rescue
+        $!.to_s == 'password supplied is not valid and GFDBA_PASSWORD is not set'
+      end
+    end
+  end
+
+  def test_connect_using_default_password
+    @p = Project.new(:la_cam_jh, :LA_CAM_JH)
+    assert_nothing_raised { @p.send :_connect_with_default_password }
+  end
+
+  def test_connect_using_invalid_default_password
+    assert_block do
+      @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ)
+      begin
+        @p.send :_connect_with_default_password
+      rescue
+        $!.to_s == 'default password for wts_work_proj is not valid and GFDBA_PASSWORD is not set'
+      end
+    end
+  end
+
+  def test_connect_using_GFDBA_PASSWORD_without_PATH
+    path = ENV[:PATH]
+    ENV[:PATH] = ENV[:PATH].split(/:/).delete_if {|path| path =~ /geoframe/}.join(':')
+    assert_block do
+      @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ)
+      begin
+        @p.send :_connect_with_gfdba_password
+      rescue
+        $!.to_s == 'unable to run proj_get_password' 
+      end
+    end
+    ENV[:PATH] = path
+  end
+
+  def test_connect_using_GFDBA_PASSWORD_without_LD_LIBRARY_PATH
+    ld_library_path = ENV[:LD_LIBRARY_PATH]
+    ENV[:LD_LIBRARY_PATH] = ENV[:LD_LIBRARY_PATH].split(/:/).delete_if {|path| path =~ /geoframe/}.join(':')
+    assert_block do
+      @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ)
+      begin
+        @p.send :_connect_with_gfdba_password
+      rescue
+        $!.to_s =~ /error while loading shared libraries/
+      end
+    end
+    ENV[:LD_LIBRARY_PATH] = ld_library_path
+  end
+
+  def test_connect_using_GFDBA_PASSWORD_without_being_set
+    assert_block do
+      @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ)
+      begin
+        @p.send :_connect_with_gfdba_password
+      rescue
+        $!.to_s == 'GFDBA_PASSWORD is not set' 
+      end
+    end
+  end
+
+  def test_connect_using_wrong_GFDBA_PASSWORD
+    ENV[:GFDBA_PASSWORD] = :bogus
+    @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ)
+    assert_block do
+      begin
+        @p.send(:_connect_with_gfdba_password)
+      rescue
+        $!.to_s == 'GFDBA_PASSWORD is not valid'
+      end
+    end
+    ENV[:GFDBA_PASSWORD] = nil
+  end
+
+  def test_connect_using_GFDBA_PASSWORD
+    ENV[:GFDBA_PASSWORD] = :midway
+    @p = Project.new(:wts_work_proj, :WTS_WORK_PROJ)
+    assert_nothing_raised { @p.send(:_connect_with_gfdba_password) }
+  end
+
   def test_bulk_server_command
-    project = Project.new(:nova_atchmrg, :NOVA_ATCHMRG)
-    assert_equal 'LOCAL:NOVA_ATCHMRG@hesz04.internal.houstonenergyinc.com:/apps/geoframe/geoframe_44_sun/bin/ctsrvr_init.csh', project.bulk_server_command
+    p = Project.new(:nova_atchmrg, :NOVA_ATCHMRG, :tigers)
+    p.connect
+    assert_equal 'LOCAL:NOVA_ATCHMRG@hesz04.internal.houstonenergyinc.com:/apps/geoframe/geoframe_44_sun/bin/ctsrvr_init.csh', p.bulk_server_command
   end
 
   def test_super_server_command
-    project = Project.new(:nova_atchmrg, :NOVA_ATCHMRG)
-    assert_equal 'LOCAL:hesz04.internal.houstonenergyinc.com:/apps/geoframe/geoframe_44_sun/bin/apu_superserver_init.csh', project.super_server_command
+    p = Project.new(:nova_atchmrg, :NOVA_ATCHMRG, :tigers)
+    p.connect
+    assert_equal 'LOCAL:hesz04.internal.houstonenergyinc.com:/apps/geoframe/geoframe_44_sun/bin/apu_superserver_init.csh', p.super_server_command
   end
 end
