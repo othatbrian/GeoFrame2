3881a611619a35f4f0ece3873dcea791d18ab2e4
Brian Stevens <brian@stevens.com>
2010-11-19 11:20:24 -0600

    Working implementations to get bulk_ and super_servers

--- lib/project.rb
+++ lib/project.rb
@@ -1,81 +1,103 @@
 require 'open3'
 require 'sequel'
 
+Sequel.single_threaded = true
+
 class Project
-  attr_accessor :name, :account
-  attr_writer :password
+	attr_accessor :name, :account
+	attr_writer :password
 
-  def initialize(name, account, password=nil)
-    @name = name
-    @account = account
-    @password = password
-  end
+	def initialize(name, account, password=nil)
+		@name = name
+		@account = account
+		@password = password
+	end
 
-  def bulk_server_command
-    @db[:parameter]
-      .select(:value_string)
-      .where(:code => 'Bulk_Server_Command')
-      .first[:value_string]
-  end
+	def bulk_server_command
+		begin
+		@db[:parameter]
+			.select(:value_string)
+			.where(:code => 'Bulk_Server_Command')
+			.first[:value_string]
+		rescue
+			connect
+			@db[:parameter]
+				.select(:value_string)
+				.where(:code => 'Bulk_Server_Command')
+				.first[:value_string]
+		end
+	end
 
-  def connect
-    @db = @password ? _connect_with_supplied_password : _connect_with_default_password
-  end
+	def connect
+		@db = @password ? _connect_with_supplied_password : _connect_with_default_password
+		@db.test_connection or raise "oops!"
+	end
 
-  def super_server_command
-    @db[:project]
-      .select(:super_server_command)
-      .first[:super_server_command]
-  end
+	def disconnect
+		@db.disconnect
+	end
 
-  private
-  def _connect_with_supplied_password
-    db = Sequel.connect("oracle://#{@account}:#{@password}@#{ENV[:TWO_TASK]}")
-    begin
-      db if db.test_connection
-    rescue
-      if @password != @name
-        begin
-          _connect_with_default_password
-        rescue
-          raise "password supplied is not valid, " + $!.to_s
-        end
-      else
-        begin
-          _connect_with_gfdba_password
-        rescue
-          raise "password supplied is not valid and " + $!.to_s
-        end
-      end
-    end
-  end
+	def super_server_command
+		begin
+			@db[:project]
+				.select(:super_server_command)
+				.first[:super_server_command]
+		rescue
+			connect
+			@db[:project]
+				.select(:super_server_command)
+				.first[:super_server_command]
+		end
+	end
 
-  def _connect_with_default_password
-    db = Sequel.connect("oracle://#{@account}:#{@account}@#{ENV[:TWO_TASK]}")
-    begin
-      db if db.test_connection
-    rescue Sequel::DatabaseConnectionError => error
-      begin
-        _connect_with_gfdba_password
-      rescue
-        raise "default password for #{@name} is not valid and " + $!.to_s
-      end
-    end
-  end
+	private
+	def _connect_with_supplied_password
+		db = Sequel.connect("oracle://#{@account}:#{@password}@#{ENV[:TWO_TASK]}")
+		begin
+			db if db.test_connection
+		rescue
+			if @password != @name
+				begin
+				  _connect_with_default_password
+				rescue
+					raise "password supplied is not valid, " + $!.to_s
+				end
+			else
+				begin
+					_connect_with_gfdba_password
+				rescue
+					raise "password supplied is not valid and " + $!.to_s
+				end
+			end
+		end
+	end
 
-  def _connect_with_gfdba_password
-    raise "GFDBA_PASSWORD is not set" unless ENV[:GFDBA_PASSWORD]
-    begin
-      Open3.popen3("proj_get_password #{@name} password") do |stdin, stdout, stderr|
-        errors = stderr.read
-        raise 'GFDBA_PASSWORD is not valid' if errors =~ /^Invalid password/m
-        stdout.read =~ /^Password =(.*)$/m
-        $1 or raise errors
-        db = Sequel.connect("oracle://#{@account}:#{$1.chomp}@#{ENV[:TWO_TASK]}")
-        db if db.test_connection
-      end
-    rescue Errno::ENOENT
-      raise 'unable to run proj_get_password'
-    end
-  end
+	def _connect_with_default_password
+		db = Sequel.connect("oracle://#{@account}:#{@account}@#{ENV[:TWO_TASK]}")
+		begin
+			db if db.test_connection
+		rescue Sequel::DatabaseConnectionError => error
+			begin
+				_connect_with_gfdba_password
+			rescue
+				raise "default password for #{@name} is not valid and " + $!.to_s
+			end
+		end
+	end
+
+	def _connect_with_gfdba_password
+		raise "GFDBA_PASSWORD is not set" unless ENV[:GFDBA_PASSWORD]
+		begin
+			Open3.popen3("proj_get_password #{@name} password") do |stdin, stdout, stderr|
+				errors = stderr.read
+				raise 'GFDBA_PASSWORD is not valid' if errors =~ /^Invalid password/m
+				stdout.read =~ /^Password =(.*)$/m
+				$1 or raise errors
+				db = Sequel.connect("oracle://#{@account}:#{$1.chomp}@#{ENV[:TWO_TASK]}")
+				db if db.test_connection
+			end
+		rescue Errno::ENOENT
+			raise 'unable to run proj_get_password'
+		end
+	end
 end
--- test/catalog_test.rb
+++ test/catalog_test.rb
@@ -2,23 +2,23 @@ require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/catalog'
 
 class CatalogTest < Test::Unit::TestCase
-  def setup
-    ENV['TWO_TASK'] = 'gfprod'
-    ENV['BASELINE_ACC'] = 'GF4'
-    @catalog = Catalog.new
-  end
+	def setup
+		ENV['TWO_TASK'] = 'gfprod'
+		ENV['BASELINE_ACC'] = 'GF4'
+		@catalog = Catalog.new
+	end
 
-  def test_initialize_without_TWO_TASK_fails
-    ENV['TWO_TASK'] = nil
-    assert_raises(RuntimeError) { Catalog.new }
-  end
+	def test_initialize_without_TWO_TASK_fails
+		ENV['TWO_TASK'] = nil
+		assert_raises(RuntimeError) { Catalog.new }
+	end
 
-  def test_initialize_without_BASELINE_ACC_fails
-    ENV['BASELINE_ACC'] = nil
-    assert_raises(RuntimeError) { Catalog.new }
-  end
+	def test_initialize_without_BASELINE_ACC_fails
+		ENV['BASELINE_ACC'] = nil
+		assert_raises(RuntimeError) { Catalog.new }
+	end
 
-  def test_projects
-    assert_equal 357, @catalog.projects.count
-  end
+	def test_projects
+		assert_equal 357, @catalog.projects.count
+	end
 end
